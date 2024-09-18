a8cbe663acd17cfb3ebeaa695b9158bb84afeed3
sarbagya42 <077bei042.sarbagya@pcampus.edu.np>
2024-09-18 19:06:16 +0545

    nepali day has been added

--- hamroPatro.jsx
+++ hamroPatro.jsx
@@ -13,7 +13,7 @@ export const className = `
   background-size:69px;
   background-repeat:no-repeat;
   background-position:top;
-  font-family: Times New Roman;
+  font-family: Arial;
   font-size: 16px;
   background-color: rgba(0, 0, 0, 0.7);
   color: white;
@@ -21,6 +21,22 @@ export const className = `
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
   width: 250px;
 `;
+
+
+const today=new Date();
+const dayIndex=today.getDay();
+
+ const nepaliWeeksDays=[
+      "आइतवार",
+      "सोमवार",
+      "मगलवार",
+      "बुधवार",
+      "बिहिवार",
+      "शुक्रवार",
+      "शनिवार"
+    ];
+
+let nepaliToDay=nepaliWeeksDays[dayIndex];
 
 //Data Parser
 function nepaliDate(data){
@@ -44,6 +60,7 @@ function yearParser(data){
 }
 
 
+
 // Render the widget
 export const render = ({ output }) => {
   const todayDate=nepaliDate(output)
@@ -52,8 +69,8 @@ export const render = ({ output }) => {
   
   return (
     <div>
-      <h4>Mero Patro</h4>
-      <h1>{nepaliMonth+" "+todayDate + ", " + nepaliYear}</h1>
+      <h3>Mac Patro</h3>
+      <h2>{nepaliMonth+" "+todayDate + ", " + nepaliYear + ", " + nepaliToDay} </h2>
     </div>
   );
 };
